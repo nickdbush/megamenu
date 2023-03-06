f36d5474499804bb647915b2e09a631cd2bc325c
Nicholas Bush <nickdbush@hotmail.co.uk>
2023-03-06 16:33:13 +0000

    Publish preview page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  appType: "mpa",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        preview: resolve(__dirname, "preview.html"),
+      },
+    },
+  },
 });
